b7d1af259b79293debab15974703e9af1fcbe64c
rismarmdni23 <rismanurainiramadlani26p2@gmail.com>
2023-06-13 10:31:23 +0800

    Membuat class main dan menjalankan program

---
+++ src/geometri/Main.java
@@ -0,0 +1,17 @@
+
+package geometri;
+
+public class Main {
+    public static void main(String[] args) {
+        double radiusLingkaran = 9.9;
+        Lingkaran lingkaran = new Lingkaran(radiusLingkaran);
+        System.out.println("Luas Lingkaran: " + lingkaran.luas());
+        System.out.println("Keliling Lingkaran: " + lingkaran.keliling());
+
+        double panjangPersegiPanjang = 9.9;
+        double lebarPersegiPanjang = 9.9;
+        PersegiPanjang persegiPanjang = new PersegiPanjang(panjangPersegiPanjang, lebarPersegiPanjang);
+        System.out.println("Luas Persegi Panjang: " + persegiPanjang.luas());
+        System.out.println("Keliling Persegi Panjang: " + persegiPanjang.keliling());
+    }
+}     
